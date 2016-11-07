ef7d23c32bfee50468c73868fe9b3730ec0cc888
Chen <chmviplove@163.com>
2016-11-07 18:04:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zxnet/xiangliao/ui/activity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.zxnet.xiangliao.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.zxnet.xiangliao.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initview();
+        initlistener();
+        initdata();
+    }
+
+    //视图
+    private void initview() {
+
+    }
+
+    //监听事件
+    private void initlistener() {
+
+    }
+
+    //数据
+    private void initdata() {
+
+    }
+}
